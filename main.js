49be95c9f44c8775292dd1aaa7d2bfd95ea0349f
Reklam41 <bonganebongza2002@gmail.com>
2024-11-20 14:53:31 +0000

    appending the pictures

--- main.js
+++ main.js
@@ -1,5 +1,20 @@
-let nextBtn = document.querySelector(".next");
-let prevBtn = document.querySelector(".prev");
+let nextDom = document.getElementById("next");
+let prevDom = document.getElementById("prev");
+
 let carouselDom = document.querySelector(".carousel");
 let listItemDom = document.querySelector(".carousel .list");
-let thumbnailsDom = document.querySelector(".carousel .thumbnails");
+let thumbnailDom = document.querySelectorAll(".carousel .thumbnails");
+
+nextDom.onclick = function () {
+  showSlider("next");
+};
+
+function showSlider(type) {
+  let itemSlider = document.querySelectorAll(".carousel .list .item");
+  let itemThumbnail = document.querySelectorAll(".carousel .thumbnails .item");
+
+  if (type === "next") {
+    listItemDom.appendChild(itemSlider[0]);
+    thumbnailDom.appendChild(itemThumbnail[0]);
+  }
+}
