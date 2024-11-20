6b84ad078f9aa98b3d2caaf4a437c92a560f929c
Reklam41 <bonganebongza2002@gmail.com>
2024-11-20 14:03:23 +0000

    some javascript

---
+++ main.js
@@ -0,0 +1,5 @@
+let nextBtn = document.querySelector(".next");
+let prevBtn = document.querySelector(".prev");
+let carouselDom = document.querySelector(".carousel");
+let listItemDom = document.querySelector(".carousel .list");
+let thumbnailsDom = document.querySelector(".carousel .thumbnails");
